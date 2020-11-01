2af89bad8998a71b8132fcefd57c25c5422f6ad6
Elias Trummer <trummer-linhard@gmx.at>
2020-11-01 13:59:02 +0100

    added default theme colors

--- csspectacular/src/Themes/Theme.ts
+++ csspectacular/src/Themes/Theme.ts
@@ -1,8 +1,39 @@
 export interface Theme {
-    readonly TestColor: ThemeColor;
+    readonly Primary_1: ThemeColor;
+    readonly Text_Primary_1: ThemeColor;
+    readonly Primary_1_Event: ThemeColor;
+    readonly Text_Primary_1_Event: ThemeColor;
+
+    readonly Primary_2: ThemeColor;
+    readonly Text_Primary_2: ThemeColor;
+    readonly Primary_2_Event: ThemeColor;
+    readonly Text_Primary_2_Event: ThemeColor;
+
+    readonly Primary_3: ThemeColor;
+    readonly Text_Primary_3: ThemeColor;
+    readonly Primary_3_Event: ThemeColor;
+    readonly Text_Primary_3_Event: ThemeColor;
+
+    readonly Primary_4: ThemeColor;
+    readonly Text_Primary_4: ThemeColor;
+    readonly Primary_4_Event: ThemeColor;
+    readonly Text_Primary_4_Event: ThemeColor;
+
+    readonly Primary_5: ThemeColor;
+    readonly Text_Primary_5: ThemeColor;
+    readonly Primary_5_Event: ThemeColor;
+    readonly Text_Primary_5_Event: ThemeColor;
 }
 
 export interface ThemeColor {
     readonly cssPropertyName: string;
     readonly color: string;
+}
+
+export enum ComponentTypes {
+    Primary_1 = 1,
+    Primary_2 = 2,
+    Primary_3 = 3,
+    Primary_4 = 4,
+    Primary_5 = 5
 }
--- csspectacular/src/Themes/ThemeTemplates/Default.ts
+++ csspectacular/src/Themes/ThemeTemplates/Default.ts
@@ -1,10 +1,111 @@
 import { Theme } from "../Theme";
 
 const __theme_default: Theme = {
-    TestColor: {
-        cssPropertyName: '--test-color',
-        color: '#17c7d2'
-    }
+    /**
+     * ===============================================================================
+     * @description Primary 1
+     * ===============================================================================
+     */
+    Primary_1: {
+        cssPropertyName: '--primary-1',
+        color: '#2ada70'
+    },
+    Text_Primary_1: {
+        cssPropertyName: '--primary-text-1',
+        color: '#fff'
+    },
+    Primary_1_Event: {
+        cssPropertyName: '--primary-event-1',
+        color: '#069840'
+    },
+    Text_Primary_1_Event: {
+        cssPropertyName: '--primary-text-event-1',
+        color: '#fff'
+    },
+    /**
+     * ===============================================================================
+     * @description Primary 2
+     * ===============================================================================
+     */
+    Primary_2: {
+        cssPropertyName: '--primary-2',
+        color: '#10abda'
+    },
+    Text_Primary_2: {
+        cssPropertyName: '--primary-text-2',
+        color: '#fff'
+    },
+    Primary_2_Event: {
+        cssPropertyName: '--primary-event-2',
+        color: '#0c7da0'
+    },
+    Text_Primary_2_Event: {
+        cssPropertyName: '--primary-text-event-2',
+        color: '#fff'
+    },
+     /**
+     * ===============================================================================
+     * @description Primary 3
+     * ===============================================================================
+     */
+    Primary_3: {
+        cssPropertyName: '--primary-3',
+        color: '#ffa822'
+    },
+    Text_Primary_3: {
+        cssPropertyName: '--primary-text-3',
+        color: '#fff'
+    },
+    Primary_3_Event: {
+        cssPropertyName: '--primary-event-3',
+        color: '#c57d0e'
+    },
+    Text_Primary_3_Event: {
+        cssPropertyName: '--primary-text-event-3',
+        color: '#fff'
+    },
+     /**
+     * ===============================================================================
+     * @description Primary 4
+     * ===============================================================================
+     */
+    Primary_4: {
+        cssPropertyName: '--primary-4',
+        color: '#f62924'
+    },
+    Text_Primary_4: {
+        cssPropertyName: '--primary-text-4',
+        color: '#fff'
+    },
+    Primary_4_Event: {
+        cssPropertyName: '--primary-event-4',
+        color: '#b00f0b'
+    },
+    Text_Primary_4_Event: {
+        cssPropertyName: '--primary-text-event-4',
+        color: '#fff'
+    },
+     /**
+     * ===============================================================================
+     * @description Primary 5
+     * ===============================================================================
+     */
+    Primary_5: {
+        cssPropertyName: '--primary-5',
+        color: '#4c4c4f'
+    },
+    Text_Primary_5: {
+        cssPropertyName: '--primary-text-5',
+        color: '#fff'
+    },
+    Primary_5_Event: {
+        cssPropertyName: '--primary-event-5',
+        color: '#262628'
+    },
+    Text_Primary_5_Event: {
+        cssPropertyName: '--primary-text-event-5',
+        color: '#fff'
+    },
 };
 
 export default __theme_default;
